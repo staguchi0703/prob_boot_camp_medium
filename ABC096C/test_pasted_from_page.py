f2140dc2581da4679fbda9e9fa255c875417e9c0
sho Taguchi <s.taguchi0703@gmail.com>
2020-04-28 00:11:26 +0900

    ABC096C

---
+++ ABC096C/test_pasted_from_page.py
@@ -0,0 +1,56 @@
+#
+from resolve import resolve
+####################################
+####################################
+# 以下にプラグインの内容をペーストする
+#  
+import sys
+from io import StringIO
+import unittest
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        print('------------')
+        print(out)
+        print('------------')
+        self.assertEqual(out, output)
+    def test_入力例_1(self):
+        input = """3 3
+.#.
+###
+.#."""
+        output = """Yes"""
+        self.assertIO(input, output)
+    def test_入力例_2(self):
+        input = """5 5
+#.#.#
+.#.#.
+#.#.#
+.#.#.
+#.#.#"""
+        output = """No"""
+        self.assertIO(input, output)
+    def test_入力例_3(self):
+        input = """11 11
+...#####...
+.##.....##.
+#..##.##..#
+#..##.##..#
+#.........#
+#...###...#
+.#########.
+.#.#.#.#.#.
+##.#.#.#.##
+..##.#.##..
+.##..#..##."""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+if __name__ == "__main__":
+    unittest.main()
