275aae2a32b48c9585b9dcf8df10788860b3b377
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-08 22:27:30 +0900

    AGC016A

--- AGC016A/resolve.py
+++ AGC016A/resolve.py
@@ -10,13 +10,18 @@ def resolve():
     unique_num_dict = collections.Counter(s)
     most_common = unique_num_dict.most_common()[0]
 
-    if most_common[1] == 1:
+    if len(unique_num_dict) == 1:
+        print(0)
+    elif most_common[1] == 1:
         print(N//2)
     else:
-        temp_list = s.strip(most_common[0])
-        res = 0
-        for item in temp_list:
-            res = max(res, len(item))
-        print(res)
+        s_set = set(s)
+        temp_min_num = 101
+        for item in s_set:
+            temp_max_num = max([len(item) for item in s.split(item)])
+            temp_min_num = min(temp_min_num, temp_max_num)
+        print(temp_min_num)
+            
+
 if __name__ == "__main__":
     resolve()
--- AGC016A/test_pasted_from_page.py
+++ AGC016A/test_pasted_from_page.py
@@ -4,3 +4,44 @@ from resolve import resolve
 ####################################
 # 以下にプラグインの内容をペーストする
 #  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        print('------------')
+        print(out)
+        print('------------')
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """serval"""
+        output = """3"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """jackal"""
+        output = """2"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """zzz"""
+        output = """0"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """whbrjpjyhsrywlqjxdbrbaomnw"""
+        output = """8"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
