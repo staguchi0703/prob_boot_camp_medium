6970db0a2079e23ef4dbdda0bb3251b5f3eae7ed
Sho Taguchi <taguchi.sho@mail.canon>
2020-07-13 12:44:49 +0900

    AGC017A

--- AGC017A/resolve.py
+++ AGC017A/resolve.py
@@ -5,9 +5,21 @@ def resolve():
     N, P = [int(item) for item in input().split()]
     As = [int(item)%2 for item in input().split()]
 
-    #　要素を01にしする
-    # P奇数なら 1のcombが奇数個の時を数え上げる
-    # P偶数なら上以外を全通りから引く
+    is_all_even = True
+    for item in As:
+        if item == 1:
+            is_all_even = False
+    
+    res = 0
+    if is_all_even:
+        if P == 0:
+            res = 2 ** N
+        else:
+            res = 0
+    else:
+        res = 2**(N-1)
+    
+    print(res)
 
 if __name__ == "__main__":
     resolve()
