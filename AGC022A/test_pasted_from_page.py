9821a88a9d1d7218f4b2fd9897f51230d9cbd991
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-22 21:18:14 +0900

    a

---
+++ AGC022A/test_pasted_from_page.py
@@ -0,0 +1,44 @@
+#
+from resolve import resolve
+####################################
+####################################
+# 以下にプラグインの内容をペーストする
+#  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """atcoder"""
+        output = """atcoderb"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """abc"""
+        output = """abcd"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """zyxwvutsrqponmlkjihgfedcba"""
+        output = """-1"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """abcdefghijklmnopqrstuvwzyx"""
+        output = """abcdefghijklmnopqrstuvx"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
