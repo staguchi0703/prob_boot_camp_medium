25f93533951b9f8ec2c8e5c8f853a7ec194db66f
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-25 20:17:43 +0900

    aa

--- ABC129C/resolve.py
+++ ABC129C/resolve.py
@@ -3,42 +3,40 @@ def resolve():
     code here
     '''
     from functools import lru_cache
+    import sys
+    sys.setrecursionlimit(10**6)
 
     N, M = [int(item) for item in input().split()]
     As = [int(input()) for _ in range(M)]
-    memo = [1 for _ in range(N+1)]
+    memo = [False for _ in range(N+1)]
 
     for item in As:
-        memo[item] = 0
+        memo[item] = True
 
     res_list = []
-    for i in range(1,N+1):
-        if i == 1 and memo[i] == 0:
-            memo[i] = 0
-            res_list.append(2)
-        elif memo[i] == 0:
-            memo[i] = 0
-            res_list.append(memo[i-1])
-        else:
-            memo[i] += memo[i-1]
-    else:
-        res_list.append(memo[N])
 
-    
+    prev = 0
+    for i in range(1,N+1):
+        if memo[i] == True:
+            res_list.append(i-1 - prev)
+            prev = i+1
+    else:
+        if memo[N] == False and memo[N] == False:
+            res_list.append(N - prev)
+
     @lru_cache
     def cal(num):
-        if num <= 1:
-            return 0
-        elif num == 2:
+        if 0 <= num <= 1:
             return 1
-        elif num == 3:
-            return 2
+        elif num == -1:
+            return 0
         else:
             return cal(num-1) + cal(num-2)
-    
+    # print(res_list)
     res = 1
     for item in res_list:
-        res *= cal(item)
+        res *= cal(item)% (10**9 + 7)
+
     print(res % (10**9 + 7))
 
 
