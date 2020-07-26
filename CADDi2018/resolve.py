1fb02d0f8bef7ecd35d55a47837d9598f258ef13
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-26 16:38:01 +0900

    ああ

--- CADDi2018/resolve.py
+++ CADDi2018/resolve.py
@@ -2,7 +2,34 @@ def resolve():
     '''
     code here
     '''
+    import collections
+    N, P = [int(item) for item in input().split()]
 
+    fact_list = [1]
+
+    cnt = 2
+    while P > 1 and cnt**2 <= P:
+        if P % cnt == 0:
+            fact_list.append(cnt)
+            P //= cnt
+        else:
+            cnt += 1
+
+    if N == 1:
+        res = P
+    else:
+        cnt_num = collections.Counter(fact_list[1:])
+        res_list = []
+        for i, v in cnt_num.items():
+            if v//N > 0:
+                res_list.append(i**(v//N))
+        
+        res = 1
+        for item in res_list:
+            res *= item
+
+    print(res)
 
 if __name__ == "__main__":
     resolve()
+# なんかおかしい思ったように動いてない
--- CADDi2018/test_pasted_from_page.py
+++ CADDi2018/test_pasted_from_page.py
@@ -17,6 +17,9 @@ class TestClass(unittest.TestCase):
         sys.stdout.seek(0)
         out = sys.stdout.read()[:-1]
         sys.stdout, sys.stdin = stdout, stdin
+        print('------------')
+        print(out)
+        print('------------')
         self.assertEqual(out, output)
 
     def test_入力例_1(self):
