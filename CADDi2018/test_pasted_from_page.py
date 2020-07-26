ef709cc6ad769195fb86087d2237eedcf916d021
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-26 16:08:33 +0900

    ddd

---
+++ CADDi2018/test_pasted_from_page.py
@@ -0,0 +1,44 @@
+#
+from resolve import resolve
+####################################
+####################################
+# 以下にプラグインの内容をペーストする
+#  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3 24"""
+        output = """2"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """5 1"""
+        output = """1"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """1 111"""
+        output = """111"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """4 972439611840"""
+        output = """206"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ codefestival2017qualA/test_pasted_from_page.py
@@ -0,0 +1,44 @@
+#
+from resolve import resolve
+####################################
+####################################
+# 以下にプラグインの内容をペーストする
+#  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """2 2 2"""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """2 2 1"""
+        output = """No"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """3 5 8"""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """7 9 20"""
+        output = """No"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
